2a46027582d60d1c1068ae446559552af78f307e
thangnd <thangnd@stageit.app>
2022-10-22 23:45:44 +0700

    update

--- build/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const router_1 = require("./users/router");
-const router = express.Router();
-router.use("/api/v1/", router_1.default);
-exports.default = router;
-//# sourceMappingURL=index.js.map
